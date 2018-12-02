7f488e989f0bba8b44438242689801742e5453b9
kHigasa <kojihigasa@gmail.com>
2018-12-02 19:19:23 +0900

    Add first template

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,14 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 
 from . import views
 
+
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/login/'), name='index'),
     path('admin/', admin.site.urls),
     path('reports/', include('reports.urls')),
     path('shifts/', include('shifts.urls')),
