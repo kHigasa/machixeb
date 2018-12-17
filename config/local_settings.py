f2786707548739c7c61022cd103a0237a5431109
kHigasa <kojihigasa@gmail.com>
2018-12-17 23:17:03 +0900

    Move db settings

--- config/local_settings.py
+++ config/local_settings.py
@@ -3,3 +3,18 @@ from .settings import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'machix_dev',
+        'USER': 'andre',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'ATOMIC_REQUESTS': True,
+    }
+}
--- config/settings.py
+++ config/settings.py
@@ -74,23 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'machix_dev',
-        'USER': 'andre',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': 5432,
-        'ATOMIC_REQUESTS': True,
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
