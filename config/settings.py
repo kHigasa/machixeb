6a73882cadcadb9d2817a0adab030e39de63de6d
kHigasa <kojihigasa@gmail.com>
2018-12-17 23:59:24 +0900

    Move db settings

--- config/settings.py
+++ config/settings.py
@@ -99,17 +99,7 @@ AUTH_USER_MODEL = 'shifts.CustomUser'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'machix_dev',
-        'USER': 'andre',
-        'PASSWORD': '',
-        # 'HOST': 'localhost',
-        # 'PORT': 5432,
-        'ATOMIC_REQUESTS': True,
-    }
-}
+django_heroku.settings(locals())
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
